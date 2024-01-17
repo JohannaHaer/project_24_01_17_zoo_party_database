b47f6260d5e0bdd87417bdc04f2ac825f52ab9a6
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-17 15:55:40 +0100

    fertig mit design

--- src/assets/components/animalItems/AnimalItems.jsx
+++ src/assets/components/animalItems/AnimalItems.jsx
@@ -4,13 +4,13 @@ const AnimalItems = ({emoji, name, species, habitat, diet, lifespan, funfact1, f
     return (
         <>
             <div className="emoji">{emoji}</div>
-            <h2>{name}</h2>
-            <h3>{species}</h3>
-            <h3>{habitat}</h3>
+            <h2>Name: {name}</h2>
+            <h3>Spezies: {species}</h3>
+            <h3>Habitat: {habitat}</h3>
             <h3>{diet}</h3>
-            <p>{lifespan}</p>
+            <p>Lebenserwartung: {lifespan} Jahre</p>
             <div className="funfacts">
-                <ul>
+                <ul className="funfactsUl">
                     <li>{funfact1}</li>
                     <li>{funfact2}</li>
                     <li>{funfact3}</li>
